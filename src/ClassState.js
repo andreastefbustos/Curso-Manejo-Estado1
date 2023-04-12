bcdc878944ad2d7a631c7930c3bcb9665eec1478
Andrea Bustos <andreastefbustos@gmail.com>
2023-04-12 12:20:59 -0400

    Métodos del ciclo de vida en React.Component

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,12 +1,37 @@
 import React from "react";
+import { Loading } from "./Loading";
 
 class ClassState extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             error: true,
+            loading: false,
         };
     }
+
+    // not used 
+    // componentWillMount()
+    // UNSAFE_componentWillMount(){
+    //     console.log("componentWillMount")
+    // };
+
+    // componentDidMount(){
+    //     console.log("componentDidMount")
+    // };
+
+    componentDidUpdate(){
+        console.log("Update")
+
+        if(this.state.loading){
+            setTimeout(() => {
+                console.log("Verify");
+                this.setState({loading: false})
+                console.log("End Verify");
+            }, 3000);
+        }
+    };
+
     render(){
         return (
             <div>
@@ -15,9 +40,12 @@ class ClassState extends React.Component{
                 {this.state.error && (
                     <p>Error: Something is wrong</p>
                 )}
+                {this.state.loading && (
+                    <Loading />
+                )}
                 <input placeholder="Security code"/>
                 <button
-                    onClick={() => this.setState(prevState => ({error: !prevState.error}))}
+                    onClick={() => this.setState({loading:true})}
                 >Verify</button>
             </div>
         );
