f21d56e4c74e379ff0b815a9e3b11296d1150329
Andrea Bustos <andreastefbustos@gmail.com>
2023-04-14 16:53:55 -0400

    3 formas de crear un reducer

---
+++ src/UseReducer.js
@@ -0,0 +1,85 @@
+//Estado compuesto
+const initialState = {
+    value: '',
+    error: false,
+    loading: false,
+    deleted: false,
+    confirmed: false,
+};
+
+//reducer recibe dos parametros, el primero el initialState, y despues va hacer el estado con todas las actualizaciones, BASE.
+
+// const reducer = (state, action) => {};
+
+//FORMA 1.
+
+//hay que validar cual es el action.type. Action va ser un objeto que tiene type, y ese type es el que nos va a decir,
+//cual es el nuevo objeto (el nuevo estado compuesto) de nuestra aplicacion.
+const reducerIf = (state,action) => {
+    if(action.type === 'ERROR'){
+        return {
+            ...state,
+            error: true,
+            loading: false,
+        }
+    } else if(action.type === 'CHECK'){
+        return {
+            ...state,
+            loading: true,
+        }
+    } else {
+        return {
+            ...state,
+        }
+    }
+};
+
+//FORMA 2. forma mas popular 
+
+const reducerSwitch = (state, action) => {
+    switch(action.type) {
+        case 'ERROR':
+            return {
+                ...state,
+                error: true,
+                loading: false,
+            };
+        case 'CHECK':
+            return {
+                ...state,
+                loading: false,
+            };
+        default:
+            return{
+                ...state,
+            };    
+    }
+};
+
+//FORMA 3. reducer object
+
+//devuelve un objeto 
+
+const reducerObject = (state) => {
+    return {
+        'ERROR': {
+            ...state,
+                error: true,
+                loading: false
+        },
+        'CHECK': {
+            ...state,
+            loading: false,
+        }
+    }
+};
+
+const reducer = (state, action) => {
+    //se le pregunta que si dentro de ese objeto existe algun objeto que se llame como action.type[]
+    if(reducerObject(state)[action.type]){
+        return reducerObject(state)[action.type]
+    } else {
+        return state;
+    }
+};
+
